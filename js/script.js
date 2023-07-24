2e8d86ce579c02e9f392f13f4faa5f29bcc6dab9
rijfas <rijfas01@gmail.com>
2023-07-24 11:54:21 +0530

    feat: direct portfolio load

--- js/script.js
+++ js/script.js
@@ -144,46 +144,18 @@ function showPortfolioDetails(portfolioItem) {
   document.querySelector("#github").innerHTML = "view on github";
 }
 
-// function toggleSuccessMessage() {
-//   hideActiveSection();
-//   document.querySelector(".message-popup").classList.toggle("active");
-// }
-
-// const contactForm = document.forms["contact-form"];
-// contactForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const AIRTABLE_WEBHOOK =
-//     "https://hooks.airtable.com/workflows/v1/genericWebhook/appk7UjBHcftYL0zF/wflzI54oVydBWyNfm/wtrOW9pRe3aVTYprv";
-//   fetch(AIRTABLE_WEBHOOK, {
-//     // Adding method type
-//     method: "POST",
-//     mode: "no-cors",
-//     // Adding body or contents to send
-//     body: JSON.stringify({
-//       name: e.target["name"].value,
-//       email: e.target["name"].value,
-//       subject: e.target["subject"].value,
-//       message: e.target["message"].value,
-//     }),
-
-//     // Adding headers to the request
-//     headers: {
-//       Accept: "*/*",
-//       "Accept-Encoding": "gzip, deflate, br",
-//       Connection: "keep-alive",
-//       "Content-type": "application/json; charset=UTF-8",
-//     },
-//   })
-//     // Converting to JSON
-//     .then(async (response) => {
-//       console.log(await response.json());
-//     });
-
-//   // Displaying results to console
-// });
-
-// const popupOkButton = document.querySelector(
-//   ".message-popup .message-inner .button-container button"
-// );
-
-// popupOkButton.addEventListener("click", (e) => toggleSuccessMessage());
+if (window.location.toString().includes("portfolio")) {
+  document.querySelector(".overlay").classList.add("active");
+  navToggler.classList.add("hide");
+  document.body.classList.add("hide-scroll-bar");
+  setTimeout(() => {
+    document
+      .querySelector("section.active")
+      .classList.remove("active", "fade-out");
+    document.querySelector("#portfolio").classList.add("active");
+    window.scrollTo(0, 0);
+    document.body.classList.remove("hide-scroll-bar");
+    navToggler.classList.remove("hide");
+    document.querySelector(".overlay").classList.remove("active");
+  }, 500);
+}
